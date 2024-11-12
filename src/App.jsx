efca22158dfc37ea3233bcc97d643bc070ad04b5
adejolaO <aogunsan@cmitsouth.org>
2024-11-12 10:27:50 -0600

    adding handleClick

--- src/App.jsx
+++ src/App.jsx
@@ -16,22 +16,27 @@ function Square({value, squareClick}){
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+    function handleClick(){
+      const nextSquares = squares.slice();
+      nextSquares[0] = "X";
+      setSquares(nextSquares);
+  }
   return (
     <>
       <div className="board-row">
-      <Square value={squares[0]}/>
-      <Square value={squares[1]}/>
-      <Square value={squares[2]}/>
+      <Square value={squares[0]} squareClick={handleClick}/>
+      <Square value={squares[1]} squareClick={handleClick}/>
+      <Square value={squares[2]} squareClick={handleClick}/>
       </div>
       <div className="board-row">
-      <Square value={squares[3]}/>
-      <Square value={squares[4]}/>
-      <Square value={squares[5]}/>
+      <Square value={squares[3]} squareClick={handleClick}/>
+      <Square value={squares[4]} squareClick={handleClick}/>
+      <Square value={squares[5]} squareClick={handleClick}/>
       </div>
       <div className="board-row">
-      <Square value={squares[6]}/>
-      <Square value={squares[7]}/>
-      <Square value={squares[8]}/>
+      <Square value={squares[6]} squareClick={handleClick}/>
+      <Square value={squares[7]} squareClick={handleClick}/>
+      <Square value={squares[8]} squareClick={handleClick}/>
       </div>
     </>
   );
