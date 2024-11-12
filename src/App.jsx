d701d890223dba9afaf6eddf9be5d2b6ec09d36b
adejolaO <aogunsan@cmitsouth.org>
2024-11-12 11:02:49 -0600

    creating history variables with usestate

--- src/App.jsx
+++ src/App.jsx
@@ -60,6 +60,10 @@ function Board() {
 }
 
 export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+  
   return (
     <div className="game">
       <div className="game-board">
