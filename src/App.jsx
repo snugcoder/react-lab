5998156a12e483a9c87a2e0d9e6e2958fb6b4e55
adejolaO <aogunsan@cmitsouth.org>
2024-11-12 10:41:41 -0600

    taking turns functionality

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,16 @@ function Square({value, squareClick}){
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
     function handleClick(i){
       const nextSquares = squares.slice();
-      nextSquares[i] = "X";
+      if (xIsNext) {
+        nextSquares[i] = "X";
+      } else {
+        nextSquares[i] = "O";
+      }
       setSquares(nextSquares);
+      setXIsNext(!xIsNext);
   }
   return (
     <>
